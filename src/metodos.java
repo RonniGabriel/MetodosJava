f0bcb71ac55bede260ac024310d7313d94c89630
RonnyGabriel <ronnigabrielg@gmail.com>
2021-03-14 20:40:39 +0100

    Commit de Metodos

--- src/metodos.java
+++ src/metodos.java
@@ -2,24 +2,59 @@ import java.util.Scanner;
 
 public class metodos {
  
-	public static void main(String[] args) {
+	// public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		// }
 
-String cadena1;
-String cadena2;
+	// String cadena1;
+//String cadena2;
 
-cadena1	= "Perro";
-cadena2 = "Perro";
+//cadena1	= "Perro";
+//cadena2 = "Perro";
 
-if (cadena1==cadena2){
-	System.out.println ("CORRECTO");
+//if (cadena1==cadena2){
+	// 	System.out.println ("CORRECTO");
 	
-}else {
-			System.out.println("INCORRECTO");
-}
-}
-}
+	// }else {
+	// 			System.out.println("INCORRECTO");
+			// // }
+//}
+//}
  
+	public static void main(String[] args) {
+		
+
+		 
+	
+	int valor;
+	System.out.println("Introducir un valor del 1 al 10");
+	Scanner teclado=new Scanner(System.in);
+	
+	switch(valor){
+		case 1: System.out.println("A");
+		break;
+		case 2: System.out.println("B");
+		break;
+		case 3: System.out.println("C");
+		break;
+		case 4: System.out.println("D");
+		break;
+		case 5: System.out.println("E");
+		break;
+		case 6: System.out.println("F");
+		break;
+		case 7: System.out.println("G");
+		break;
+		case 8: System.out.println("H");
+		break;
+		case 9: System.out.println("I");
+		break;
+		case 10: System.out.println("J");
+		break;
+	}
+	}
+	
+	}
+}
 
